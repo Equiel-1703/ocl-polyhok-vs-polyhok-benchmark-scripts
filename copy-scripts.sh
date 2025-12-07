ada637c95a0f57baeedb69f5277810d17b8bdd52
Equiel-1703 <henriquegabriel22.hg@gmail.com>
2025-12-07 19:02:31 -0300

    Add copy-scripts.sh to automate script copying for benchmarks

---
+++ copy-scripts.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Copy the benchmark to OCL-Polyhok and Polyhok directorie
+cp ocl-polyhok-polyhok-script.sh ../ocl-polyhok/
+cp ocl-polyhok-polyhok-script.sh ../poly_hok/
+
+# Copy OpenCL script to OCL-Polyhok directory
+cp opencl-script.sh ../ocl-polyhok/
+
+# Copy CUDA script to Polyhok directory
+cp cuda-script.sh ../poly_hok/
